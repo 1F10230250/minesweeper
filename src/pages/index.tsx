0178964e1a8f82b958cee39e6e10397163724dca
1F10230250 <s1f102302506@iniad.org>
2023-06-16 03:12:41 +0900

    ヒント１まで

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,35 @@
 import styles from './index.module.css';
-
+import { useState } from 'react';
 const Home = () => {
+  const directions = [[-1, 0], [-1, -1], [0, -1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1]];
+  const [userInputs, setUserInputs] = useState<(0|1|2|3)[][]>([
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+  ])
+  const bombCount=10;
+  const [bombMap, setbombMap]=useState([
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+    [0,0,0,0,0,0,0,0,0,0,],
+  ])
+  const isPlaying =userInputs.some((row)=>row.some((input)=>input !==0));
+  const isFailure =userInputs.some((row,y)=>
+    row.some((input,x)=>input===1&&bombMap[y][x]===1) 
+  );
+  const board:number[][]=[];
   return (
     <div className={styles.container}>
       <main className={styles.main}>
