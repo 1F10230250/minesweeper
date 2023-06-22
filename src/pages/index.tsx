e7922a7c04444f6564394526b326943fa6ae18d4
1F10230250 <s1f102302506@iniad.org>
2023-06-22 18:39:43 +0900

    爆弾

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,25 +44,6 @@ const Home = () => {
   const isFailure = userInputs.some((row, y) =>
     row.some((input, x) => input === 1 && bombMap[y][x] === 1)
   );
-  let bomb_number=0
-    for (let yb=0; yb<=8;yb+=1){
-      for (let xb=0; xb<=8; xb+=1){
-        if (bombMap[yb][xb]===1){
-          bomb_number+=1
-        }
-      }
-    }
-  let bombNumber=0
-  if (isPlaying===true){
-      while (bomb_number<10){
-        var random_y = Math.floor( Math.random() * 9 );
-        var random_x = Math.floor( Math.random() * 9 );
-        if (userInputs[random_y][random_x]===0){
-          bombMap[random_y][random_x]=1
-        }
-      }
-  }
-  
   
     //-1->石
   //0->画像なしセル
@@ -91,6 +72,32 @@ const Home = () => {
       }
       setUserInputs(newUserInputs);
     }
+    let bomb_number=0
+    for (let yb=0; yb<=8;yb+=1){
+      for (let xb=0; xb<=8; xb+=1){
+        if (bombMap[yb][xb]===1){
+          bomb_number+=1
+        }
+      }
+    }
+  let bombNumber=0
+  if (isPlaying===true){
+      while (bomb_number<10){
+        var random_y = Math.floor( Math.random() * 9 );
+        var random_x = Math.floor( Math.random() * 9 );
+        if (userInputs[random_y][random_x]===0){
+          if (bombMap[random_y][random_x]=1){
+            for (let yb=0; yb<=8;yb+=1){
+              for (let xb=0; xb<=8; xb+=1){
+                if (bombMap[yb][xb]===1){
+                  bomb_number+=1
+                }
+              }
+            }
+          }
+        }
+      }
+  }
     for (let yaa=0; yaa<=8; yaa+=1){
       for (let xaa=0; xaa<=8; xaa+=1){
         if (userInputs[yaa][xaa]===1){
@@ -104,36 +111,37 @@ const Home = () => {
               }
           }
           if(bombMap[yaa][xaa]!==1){
-            for (let y_a = -1; y_a<1; y_a+=1){
-              for (let x_a =-1; x_a<1;x_a+=1){
-                if (bombMap[yaa+y_a][xaa+y_a]===1){
-                  if (bombNumber===0){
-                    board[yaa][xaa]=0
-                  }
-                  if (bombNumber===1){
-                    board[yaa][xaa]=1
-                  }
-                  if (bombNumber===2){
-                    board[yaa][xaa]=2
-                  }
-                  if (bombNumber===3){
-                    board[yaa][xaa]=3
-                  }
-                  if (bombNumber===4){
-                    board[yaa][xaa]=4
-                  }
-                  if (bombNumber===5){
-                    board[yaa][xaa]=5
-                  }
-                  if (bombNumber===6){
-                    board[yaa][xaa]=6
-                  }
-                  if (bombNumber===7){
-                    board[yaa][xaa]=7
-                  }
-                  if (bombNumber===8){
-                    board[yaa][xaa]=8
-                  }
+            for (let y_a = -1; y_a<=1; y_a+=1){
+              for (let x_a =-1; x_a<=1;x_a+=1){
+                if (bombMap[yaa+y_a][xaa+x_a]===1){
+                  bombNumber+=1
+                }
+                if (bombNumber===0){
+                  board[yaa][xaa]=0
+                }
+                if (bombNumber===1){
+                  board[yaa][xaa]=1
+                }
+                if (bombNumber===2){
+                  board[yaa][xaa]=2
+                }
+                if (bombNumber===3){
+                  board[yaa][xaa]=3
+                }
+                if (bombNumber===4){
+                  board[yaa][xaa]=4
+                }
+                if (bombNumber===5){
+                  board[yaa][xaa]=5
+                }
+                if (bombNumber===6){
+                  board[yaa][xaa]=6
+                }
+                if (bombNumber===7){
+                  board[yaa][xaa]=7
+                }
+                if (bombNumber===8){
+                  board[yaa][xaa]=8
                 }
               }
             }
