7869c1aa4209a11ecf097ad86212c134ac118898
1F10230250 <1F10230250@users.noreply.github.com>
2024-06-21 14:50:10 +0000

    deploy: 7c55fadc56af2999f37f5c6aacf4c2002b354c63

---
+++ _next/static/OK4Y_I3mS6tfsH2Cerx_U/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4bd89b93cc85f443.css","static/chunks/pages/index-ad0601f2478e1f19.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/upubdvcp-v9F8HXf520bm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/4bd89b93cc85f443.css","static/chunks/pages/index-43c7884112c4b033.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
